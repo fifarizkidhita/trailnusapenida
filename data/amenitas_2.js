30688dab2cc87b6b0cd3c5500133be82cb63bc5a
fifarizkidhita <79030235+fifarizkidhita@users.noreply.github.com>
2021-02-15 10:13:08 +0700

    Add files via upload

---
+++ data/amenitas_2.js
@@ -0,0 +1,25 @@
+var json_amenitas_2 = {
+"type": "FeatureCollection",
+"name": "amenitas_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama": "ATM BRI Pelabuhan Toyapakeh", "Longitude": 115.48863, "Latitude": -8.68185 }, "geometry": { "type": "Point", "coordinates": [ 115.48863, -8.681854 ] } },
+{ "type": "Feature", "properties": { "Nama": "Masjid Al-Imron", "Longitude": 115.48807, "Latitude": -8.68207 }, "geometry": { "type": "Point", "coordinates": [ 115.488068, -8.682072 ] } },
+{ "type": "Feature", "properties": { "Nama": "Gamat Bay Diving Spot", "Longitude": 115.46592, "Latitude": -8.69964 }, "geometry": { "type": "Point", "coordinates": [ 115.465921, -8.699644 ] } },
+{ "type": "Feature", "properties": { "Nama": "Pura Ulundanu Penida", "Longitude": 115.45934, "Latitude": -8.71513 }, "geometry": { "type": "Point", "coordinates": [ 115.459342, -8.715133 ] } },
+{ "type": "Feature", "properties": { "Nama": "Pulau Batu Mejinong", "Longitude": 115.45642, "Latitude": -8.71458 }, "geometry": { "type": "Point", "coordinates": [ 115.456423, -8.714575 ] } },
+{ "type": "Feature", "properties": { "Nama": "Pura Dalem Banjar Adat Senangka", "Longitude": 115.4953, "Latitude": -8.70797 }, "geometry": { "type": "Point", "coordinates": [ 115.495297, -8.707971 ] } },
+{ "type": "Feature", "properties": { "Nama": "Warung Petrol", "Longitude": 115.56185, "Latitude": -8.75617 }, "geometry": { "type": "Point", "coordinates": [ 115.561848, -8.756166 ] } },
+{ "type": "Feature", "properties": { "Nama": "Puskesmas Desa Tanglad", "Longitude": 115.58188, "Latitude": -8.77271 }, "geometry": { "type": "Point", "coordinates": [ 115.581884, -8.772709 ] } },
+{ "type": "Feature", "properties": { "Nama": "Rumah Pohon Molenteng", "Longitude": 115.61691, "Latitude": -8.77831 }, "geometry": { "type": "Point", "coordinates": [ 115.616911, -8.778309 ] } },
+{ "type": "Feature", "properties": { "Nama": "Pura Puseh Dusun Ampel ", "Longitude": 115.60531, "Latitude": -8.75521 }, "geometry": { "type": "Point", "coordinates": [ 115.605313, -8.755214 ] } },
+{ "type": "Feature", "properties": { "Nama": "Pura Atuh", "Longitude": 115.62065, "Latitude": -8.77343 }, "geometry": { "type": "Point", "coordinates": [ 115.62065, -8.77343 ] } },
+{ "type": "Feature", "properties": { "Nama": "Pertamini", "Longitude": 115.60012, "Latitude": -8.72795 }, "geometry": { "type": "Point", "coordinates": [ 115.600118, -8.727951 ] } },
+{ "type": "Feature", "properties": { "Nama": "ATM BRI Batununggul", "Longitude": 115.55802, "Latitude": -8.67362 }, "geometry": { "type": "Point", "coordinates": [ 115.558022, -8.673617 ] } },
+{ "type": "Feature", "properties": { "Nama": "Pura dalem Cemara Sakti", "Longitude": 115.57138, "Latitude": -8.67738 }, "geometry": { "type": "Point", "coordinates": [ 115.571375, -8.677379 ] } },
+{ "type": "Feature", "properties": { "Nama": "PERTAMINA 56.807.01", "Longitude": 115.57182, "Latitude": -8.67768 }, "geometry": { "type": "Point", "coordinates": [ 115.571815, -8.677675 ] } },
+{ "type": "Feature", "properties": { "Nama": "APS Money Changer", "Longitude": 115.55393, "Latitude": -8.67368 }, "geometry": { "type": "Point", "coordinates": [ 115.553931, -8.673677 ] } },
+{ "type": "Feature", "properties": { "Nama": "RS Pratama Nusa Penida", "Longitude": 115.49499, "Latitude": -8.67654 }, "geometry": { "type": "Point", "coordinates": [ 115.494988, -8.676544 ] } },
+{ "type": "Feature", "properties": { "Nama": "Apotik Nusa Usada Santi Farma", "Longitude": 115.49456, "Latitude": -8.67687 }, "geometry": { "type": "Point", "coordinates": [ 115.494562, -8.676871 ] } }
+]
+}
---
+++ data/nuspen_titik_destinasi_4.js
@@ -0,0 +1,16 @@
+var json_nuspen_titik_destinasi_4 = {
+"type": "FeatureCollection",
+"name": "nuspen_titik_destinasi_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama": "Kelingking Beach", "Longitude": 115.47317, "Latitude": -8.75091 }, "geometry": { "type": "Point", "coordinates": [ 115.473169, -8.750911 ] } },
+{ "type": "Feature", "properties": { "Nama": "Umah Dewa Restaurant", "Longitude": 115.47664, "Latitude": -8.75072 }, "geometry": { "type": "Point", "coordinates": [ 115.476641, -8.750719 ] } },
+{ "type": "Feature", "properties": { "Nama": "Gamat Bay", "Longitude": 115.47595, "Latitude": -8.70209 }, "geometry": { "type": "Point", "coordinates": [ 115.475945, -8.70209 ] } },
+{ "type": "Feature", "properties": { "Nama": "Crystal Beach Sea Sunset", "Longitude": 115.4592, "Latitude": -8.71554 }, "geometry": { "type": "Point", "coordinates": [ 115.459197, -8.715541 ] } },
+{ "type": "Feature", "properties": { "Nama": "Thousand Island", "Longitude": 115.61693, "Latitude": -8.77878 }, "geometry": { "type": "Point", "coordinates": [ 115.61693, -8.778779 ] } },
+{ "type": "Feature", "properties": { "Nama": "Atuh Beach", "Longitude": 115.62246, "Latitude": -8.77375 }, "geometry": { "type": "Point", "coordinates": [ 115.622462, -8.773751 ] } },
+{ "type": "Feature", "properties": { "Nama": "Pura Goa Giri Putri", "Longitude": 115.58612, "Latitude": -8.70895 }, "geometry": { "type": "Point", "coordinates": [ 115.58612, -8.708952 ] } },
+{ "type": "Feature", "properties": { "Nama": "Pasar Sengol Mentigi", "Longitude": 115.55631, "Latitude": -8.67322 }, "geometry": { "type": "Point", "coordinates": [ 115.556308, -8.673222 ] } },
+{ "type": "Feature", "properties": { "Nama": "Pura Dalem Penataran Ped", "Longitude": 115.51825, "Latitude": -8.6779 }, "geometry": { "type": "Point", "coordinates": [ 115.518249, -8.677901 ] } }
+]
+}
